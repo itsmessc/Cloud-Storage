f3de1b3ef86c3da8361c4ee8fd1f50c604c307b6
Sanganwar Sai Charan <122984649+itsmessc@users.noreply.github.com>
2024-11-13 18:28:10 +0530

    Delete cs directory

--- cs/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from '../reducers'; // Assuming you have a single reducer file
-
-const store = configureStore({
-  reducer: rootReducer
-});
-
-export default store;
